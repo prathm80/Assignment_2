d49d9b22edd0bc5ceda19b307deef7b0392e2412
Prathmesh kenekar <115297540+prathm80@users.noreply.github.com>
2023-03-12 23:26:25 +0530

    Add files via upload

---
+++ Assignment_2_2.py
@@ -0,0 +1,26 @@
+# Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+
+# Sample Output : {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 
+# 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
+
+# python program to get a list from user in incresing order by the last element in each tuple
+
+
+def last(n):
+     return n[-1]
+def sort(tuples):
+     return sorted(tuples, key = last)
+
+a= input("Enter a list of tuples:")
+print("sorted:")
+print(sort(a))
+
+
+# create the dict whose alphabet from a-z
+
+dict = {}
+
+for i in range (65, 65 + 26):
+    dict[chr(i)] = i
+    
+print(dict)
